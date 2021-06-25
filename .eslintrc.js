762e1619e8aebe51acdeffd5393e3cc342caef38
Justin Barca <justinbarca@gmail.com>
2021-06-25 12:58:56 -0400

    missed eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'max-len': [2, { code: 120 }],
+    // https://stackoverflow.com/a/65768375/2780052
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2621
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
