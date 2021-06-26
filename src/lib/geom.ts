db33c75a475d72551c8b259b560aeb4fb4fd925e
Justin Barca <justinbarca@gmail.com>
2021-06-26 19:15:54 -0400

    test castCoordToPoint2D

--- src/lib/geom.ts
+++ src/lib/geom.ts
@@ -9,15 +9,35 @@ const eqNumber: eq.Eq<number> = { equals: (x, y) => x === y };
 
 export const eqPoint: eq.Eq<Point2D> = array.getEq(eqNumber);
 
-
 // self-comparison excludes NaN as NaN !== NaN
 // eslint-disable-next-line no-self-compare
-const isNumber = (val: any) => typeof val === 'number' && val === val;
+const isFiniteNumber = (val: any) => val === val && Math.abs(val) !== Infinity;
 
+const isNumber = (val: any) => typeof val === 'number';
 const isPointLike = (coord: Coord): coord is PointLike => isNumber((coord as PointLike).x)
   && isNumber((coord as PointLike).y);
 
+
+// note JSON.stringify([NaN]) === "[null]", using "accurate" replacer from util.ts adds quotes to e.g. ["NaN"]
+export const stringifyCoord = (coord: Coord) => (isPointLike(coord)
+  ? `{x:${coord.x}, y:${coord.y}}` : `[${coord.join(',')}]`);
+
+const assertPointValuesValid = (coord: Coord) => {
+  const coordIsPointLike = isPointLike(coord);
+  const x = coordIsPointLike ? (coord as PointLike).x : coord[0];
+  const y = coordIsPointLike ? (coord as PointLike).y : coord[1];
+  const xIsValid = isFiniteNumber(x);
+  const yIsValid = isFiniteNumber(y);
+  if (!xIsValid || !yIsValid) {
+    throw new Error(
+      `castCoordToPoint2D failed to cast parameter ${stringifyCoord(coord)
+      }: both coordinate values must not be NaN or +/-Infinity`,
+    );
+  }
+};
+
 export const castCoordToPoint2D = (coord: Coord): Point2D => {
+  assertPointValuesValid(coord);
   if (isPointLike(coord)) {
     const { x, y } = coord as PointLike;
     return [x, y];
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,18 @@
+function accurateNumberReplacer(_: any, value: any) {
+  if (value === Infinity) {
+    return 'Infinity';
+  } if (value === -Infinity) {
+    return '-Infinity';
+    // eslint-disable-next-line no-self-compare
+  } if (value !== value) {
+    return 'NaN';
+  }
+  return value;
+}
+
 export const prettyPrintIdentityWithPrefix = (prefix: string) => (obj: any) => {
   // eslint-disable-next-line no-console
-  console.log(prefix, JSON.stringify(obj, null, 2));
+  console.log(prefix, JSON.stringify(obj, accurateNumberReplacer, 2));
   return obj;
 };
+
