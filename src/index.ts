b5d56b21eed57463be2c7ed6f212a92ac637143c
Justin Barca <justinbarca@gmail.com>
2021-06-27 11:18:19 -0400

    favor pushCommands over pushCommand

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {
-  commandArrayToPathD, COMMAND_FACTORY, reformatPathD, isBezierCommand, isDestinationCommand, pushCommand,
+  commandArrayToPathD, COMMAND_FACTORY, reformatPathD, isBezierCommand, isDestinationCommand, pushCommands,
 } from './lib/command';
 export { prettyPrintIdentityWithPrefix } from './lib/util';
